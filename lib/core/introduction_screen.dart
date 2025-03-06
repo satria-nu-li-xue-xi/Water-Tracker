2f08688e97264a2a3af30dfea267a736a557ec93
nenz24 <danendracool08@gmail.com>
2025-03-06 17:02:09 +0800

    add introduction screen

---
+++ lib/core/introduction_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:project_onepercent/features/Profile/presentation/pages/profile_pages.dart';
+
+class Introduction extends StatelessWidget {
+  const Introduction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IntroductionScreen(
+      pages: [
+        PageViewModel(
+            decoration: PageDecoration(pageColor: Colors.lightBlueAccent),
+            title: 'Drops Water Tracker',
+            body: 'Stay hydrated and track your daily water intake',
+            image: Center(
+              child: Image.asset('images/ISpage1.png'),
+            )),
+        PageViewModel(
+            title: 'Drops Water Tracker',
+            body: 'Stay hydrated and track your daily water intake',
+            image: Center(
+              child: Image.asset('images/ISpage1.png'),
+            )),
+        PageViewModel(
+            title: 'Drops Water Tracker',
+            body: 'Stay hydrated and track your daily water intake',
+            image: Center(
+              child: Image.asset('images/ISpage1.png'),
+            ))
+      ],
+      showBackButton: true,
+      back: Icon(Icons.arrow_back_ios),
+      showNextButton: true,
+      next: Text('Next'),
+      done: const Text('Done'),
+      onDone: () {
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ProfileView(),
+            ));
+      },
+    );
+  }
+}
